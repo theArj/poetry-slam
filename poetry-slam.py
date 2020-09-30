bbd0835a14b87dce3a60ba058e1a92586f23b671
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-09-30 16:54:25 -0700

    Created barebones functions outline

--- poetry-slam.py
+++ poetry-slam.py
@@ -1,19 +1,38 @@
 # 1. You will store your poem in a .txt file. Feel free to use any poem of your choice!
 
+# filename = "poem.txt"
+# infile = open(filename, "r")
+
+# with open(poem.txt, 'r') as outfile:
+#     outfile.write()
+
 # 2. Create a function called get_file_lines():
 
 #     It should have 1 parameter called filename, which is a string of the filename.
 #     It should return a list of strings containing the lines of the file.
+
+
+def get_file_lines(filename):
+    return
 
 # 3. Create a function called lines_printed_backwards():
 
 #     It should have 1 parameter called lines_list, which is a list of strings containing lines of your poem.
 #     It should print the lines of the poem in reverse. Include the original line number at the beginning of each line.
 
+
+def lines_printed_backwards()(lines_list):
+    print()
+
 # 4. Create a function called lines_printed_random():
 
 #     It should have 1 parameter called lines_list, which is a list of strings containing lines of your poem.
-#     t should print the lines of your poem in randomly order. Repeats are ok, but make sure the number of lines printed is equal to the original lines in the poem(Line numbers do not need to be printed.) Hint: try using a loop and randint() from the random module.
+#     It should print the lines of your poem in randomly order. Repeats are ok, but make sure the number of lines 
+#     printed is equal to the original lines in the poem(Line numbers do not need to be printed.) Hint: try 
+#     using a loop and randint() from the random module.
+
+def lines_printed_random(lines_list):
+    print()
 
 # 5. Create a function called lines_printed_custom():
 
@@ -21,4 +40,9 @@
 #     It should print the poem in a unique way, be creative!
 #     Make sure that you carefully comment your custom function so it’s clear what it does.
 
-# 6. Your final submitted code should use the four functions(get_file_lines(), lines_printed_backwards(), lines_printed_random(), and lines_printed_custom()) you wrote. It should print out the poem contained in your text file backwards, random, and custom to the terminal.
+def lines_printed_custom(lines_list):
+    print()
+
+# 6. Your final submitted code should use the four functions(get_file_lines(), lines_printed_backwards(), lines_printed_random(),
+#    and lines_printed_custom()) you wrote. It should print out the poem contained in your text file backwards, random, and 
+#    custom to the terminal.
